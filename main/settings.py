d205e1714d983ff7c80da7d4004166a003bd11aa
Kees den Heijer <kees.denheijer@deltares.nl>
2017-06-13 13:25:30 +0200

    requirements updated

--- main/settings.py
+++ main/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'images',
     'highcharts',
     'rest_framework',
+    'django_filters',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
