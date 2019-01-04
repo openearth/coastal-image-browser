8b28dc6f44e7464b65e083f5064f162de2049caa
Kees den Heijer <c.denheijer@tudelft.nl>
2019-01-04 15:01:47 +0100

    image filter changed to class based on and choices implemented

--- images/views.py
+++ images/views.py
@@ -7,9 +7,17 @@ from .serializers import SiteSerializer, ImageSerializer
 from .models import Images, Sites
 
 from rest_framework.pagination import PageNumberPagination
-import django_filters.rest_framework
+from django_filters import FilterSet, ChoiceFilter, rest_framework
+from django.db.models import Max
 
 
+# obtain site choices from site table
+SITE_CHOICES = Sites.objects.values_list('site', 'description')
+# construct camera choices ranging from 1 to the maximum camera number (because distinct is not supported by the database backend)
+CAMERA_CHOICES = [(cam_num, cam_num) for cam_num in range(1, Images.objects.values('camera').aggregate(Max('camera'))['camera__max']+1)]
+
+
+# for documentation purposes: the docstring is used as title in the API root view
 class ImagesAPIRootView(routers.APIRootView):
     """
     Coastal Images API
@@ -20,6 +28,18 @@ class LargeResultsSetPagination(PageNumberPagination):
     page_size = 100
     page_size_query_param = 'page_size'
     max_page_size = 1000
+
+
+class ImageFilter(FilterSet):
+    """
+    Image filter class to provide sensible choices
+    """
+    site = ChoiceFilter(choices=SITE_CHOICES)
+    camera = ChoiceFilter(choices=CAMERA_CHOICES)
+
+    class Meta:
+        model = Images
+        fields = ('site', 'camera', 'dayminute', 'epoch')
 
 
 # ViewSets define the view behavior.
@@ -36,8 +56,8 @@ class ImageViewSet(viewsets.ModelViewSet):
     serializer_class = ImageSerializer
     http_method_names = ['get',]
     pagination_class = LargeResultsSetPagination
-    filter_backends = (django_filters.rest_framework.DjangoFilterBackend,)
-    filter_fields = ('site', 'camera', 'dayminute', 'epoch')
+    filter_backends = (rest_framework.DjangoFilterBackend,)
+    filter_class = ImageFilter
     ordering_fields = ('site', 'camera', 'epoch')
 
 
